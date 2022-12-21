1601c9c156c8b8fd345ac52ebf2393ba8e1cb9a6
Hanz <hanz@godot.id>
2022-12-21 10:16:40 +0700

    feat: Added setup

---
+++ setup.py
@@ -0,0 +1,25 @@
+import sys
+from setuptools import find_packages, setup
+
+from vcompy.version import __version__
+
+with open('README.md', 'r') as f:
+	README = f.read()
+
+requires = [
+	'Pillow==9.3.0'
+]
+
+setup(
+	name='vcompy',
+	version=__version__,
+	author='Hanz <hanz@godot.id>',
+	description='Video compiler',
+	long_description=README,
+	url='https://github.com/dot-six/vcompy',
+	license='MIT License',
+	classifiers=[]
+	keywords='video editing ffmpeg',
+	packages='vcompy',
+	install_requires=requires
+)
