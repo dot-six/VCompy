871214f103fd70dc4d35f3f54834a3a8f3a40abe
Hanz <hanz@godot.id>
2022-12-21 10:50:35 +0700

    feat: Added Video.py

---
+++ vcompy/Video.py
@@ -0,0 +1,24 @@
+import numpy as np
+import imageio.v3 as iio
+
+from .Media import Media
+
+class Video(Media):
+	def __init__(self, fps=24, **kwargs):
+		super.__init__(**kwargs)
+
+		self.fps = fps
+		self.img = None
+
+	@staticmethod
+	def from_file(path):
+		v = Video()
+		v.img = iio.imopen(path, 'r')
+
+		return v
+
+	def get_frame(self, i):
+		if self.img is None:
+			raise Exception('Video self.img unset')
+
+		return self.img.read(index=i)
