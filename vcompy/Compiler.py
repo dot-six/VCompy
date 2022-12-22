07f33feb41d7389fa23eaa8095c30a98fb1a2d18
Hanz <hanz@godot.id>
2022-12-22 17:30:22 +0700

    fix(Compiler): Insert subclips instead of appending to the end of list

--- vcompy/Compiler.py
+++ vcompy/Compiler.py
@@ -49,9 +49,9 @@ class Compiler:
 	def get_clips_in_frame(self, i):
 		clips = list()
 
-		for clip in self.clips:
+		for clipIndex, clip in enumerate(self.clips):
 			if clip.start <= i and i < clip.start + clip.duration:
-				clips.append(clip)
+				clips.append((clipIndex, clip))
 
 		return clips
 
@@ -80,7 +80,7 @@ class Compiler:
 			clipsInFrame = self.get_clips_in_frame(self.frameIndex)
 			newCache = []
 
-			for clip in clipsInFrame:
+			for clipIndex, clip in clipsInFrame:
 				clipType = type(clip)
 
 				# Check if clip not yet in cache
@@ -102,8 +102,8 @@ class Compiler:
 						# Append to cache
 						self.pagecache.append(subclip)
 						newCache.append(subclip)
-						# Append to clips
-						self.clips.append(subclip)
+						# Insert to clips
+						self.clips.insert(clipIndex, subclip)
 
 						# Remove part of original
 						clip.start = subclip.start + subclip.duration
@@ -133,13 +133,15 @@ class Compiler:
 						clip.position, clip.text,
 						fill=clip.color, font=_font, anchor=clip.anchor, align=clip.align
 					)
+
 			#frame.save(f"{self.TMP_FOLDER}-img-seq/{self.frameIndex}.png", format="PNG")
 			avframe = av.VideoFrame.from_image(frame)
 			frame.close()
 
 			# Check for outdated cache
 			for cache in self.pagecache:
-				if not cache in clipsInFrame and not cache in newCache:
+				# TODO: Tidy this
+				if not cache in map(lambda t: t[1], clipsInFrame) and not cache in newCache:
 					self.pagecache.remove(cache)
 					self.clips.remove(cache)
 
